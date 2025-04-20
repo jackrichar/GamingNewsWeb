42e46a8b4213ff4c4e56b0050eadf6cb7aa2fdab
mohammadmahdi-khaje <166861811+mohammadmahdi-khaje@users.noreply.github.com>
2025-04-20 11:02:14 +0330

    .

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./AppStyle.scss";
+import Cart from "../component/cart/Cart";
 
 const App = () => {
-    return(
-        <div className="App-Background">
-        </div>
-    );
+  return (
+    <div className="App-Background">
+      <Cart />
+    </div>
+  );
 };
 
 export default App;
--- src/component/cart/Cart.jsx
+++ src/component/cart/Cart.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import style from "./Cart.module.scss";
+import RDR from "../../Assets/image/Untitled.png";
 
 function Cart() {
   return (
     <div className={style.main}>
       <div className={style.main_container}>
-        <div className={style.main_image}></div>
+        <div className={style.main_image_container}>
+          <img className={style.main_image} src={RDR}></img>
+        </div>
       </div>
-      <div className={style.main_text}>Red Dead Redemption 2</div>
     </div>
   );
 }
