6ea37d1684cc85bf5914f9cd1f526da1f624d636
jackrichar <jackrichardsaleh@gmail.com>
2025-04-24 14:47:25 +0330

    Change App Component V-3

--- src/App/App.js
+++ src/App/App.js
@@ -15,8 +15,8 @@ const App = () => {
         <div className="App-Navigation" style={ NavigationStatus ? {width: "13rem"} : {width: "55px"}}>
             <Navigation setNavigationStatus={setNavigationStatus}/>
         </div>
-        <div className="App-Content">
-            <SlideShow />
+        <div className="App-Content" style={ NavigationStatus ? {width: "calc(100% - 13rem)"} : {width: "calc(100% - 55px)"}}>
+
         </div>
     </div>
   );
