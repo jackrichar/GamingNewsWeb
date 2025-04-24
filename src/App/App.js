88c1e7575855b6179e41df472466160a4c91ee10
jackrichar <jackrichardsaleh@gmail.com>
2025-04-24 14:24:32 +0330

    Change App Component V-1

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 import "./AppStyle.scss";
 
 // Import Component
 import Navigation from "../component/navigation/Navigatin";
+import Slideshow from "../component/SlideShow/SlideShow";
 
+// Import Context
 
 
 
 const App = () => {
+    const [NavigationStatus, setNavigationStatus] = useState(false);
+
   return (
     <div className="App-Background">
-        <div className="App-Navigation">
-            <Navigation />
+        <div className="App-Navigation" style={ NavigationStatus ? {width: "13rem"} : {width: "55px"}}>
+            <Navigation setNavigationStatus={setNavigationStatus}/>
         </div>
         <div className="App-Content">
-
+            <Slideshow/>
         </div>
     </div>
   );
