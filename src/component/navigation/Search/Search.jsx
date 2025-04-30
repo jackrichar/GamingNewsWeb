0517244643634b0ebee3a3c61de2e06ce7384e29
jackrichar <jackrichardsaleh@gmail.com>
2025-04-30 11:36:47 +0330

    Complete Navigation V-2

--- src/component/navigation/Search/Search.jsx
+++ src/component/navigation/Search/Search.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import './SearchStyle.scss';
 
 // Import SVG
@@ -6,10 +6,11 @@ import {ReactComponent as CloseIcon} from '../../../Assets/Icon/Close.svg';
 import {ReactComponent as SearchIcon} from '../../../Assets/Icon/search.svg';
 
 const Search = ({OpenModal, setOpenModal}) => {
-    const ClickOut = useRef(null);
-
+    const [Empty, setEmpty] = useState(false);
+    const [Value, setValue] = useState('');
     /////////////////////////////////////////////////////////
-
+    const ClickOut = useRef(null);
+    /////////////////////////////////////////////////////////
     useEffect(() => {
         function handleClickOutside(event) {
             if (
@@ -25,25 +26,38 @@ const Search = ({OpenModal, setOpenModal}) => {
             document.removeEventListener("mousedown", handleClickOutside);
         };
     }, [setOpenModal]);
-
+    useEffect(()=>{
+        if (Value === "") {
+            setEmpty(true);
+        }else if(Value !== "") {
+            setEmpty(false);
+        }
+    }, [Value]);
+    useEffect(() => {
+        if(!OpenModal){
+            setValue("");
+        }
+    }, [OpenModal]);
     ////////////////////////////////////////////////////////
+
+    // HTML
     return (
         <div className={`Search-Modal-Background ${OpenModal ? 'Modal-Active' : 'Modal-DeActive'}`}>
-            <div className="Search-Modal" ref={ClickOut} >
-                <div className="Search-Modal-Close-Icon">
-                    <button className="Close-Button" onClick={()=> setOpenModal(false)}>
-                        <CloseIcon className="Close-Icon"/>
+            <div className="Search-Parent">
+                <div className={`Search-box-Background ${!Empty ? "Change-Search-Box" : ""}`} ref={ClickOut}>
+                    <button>
+                        <SearchIcon className="Search-Icon"/>
                     </button>
+                    <input
+                        type="search"
+                        placeholder="جستوجو"
+                        onChange={(e) => {
+                            setValue(e.target.value);
+                        }}
+                        value={Value}
+                    />
                 </div>
-                <div className="Search-Modal-Search-Box">
-                    <div className="Search-box-Background">
-                        <button>
-                            <SearchIcon className="Search-Icon"/>
-                        </button>
-                        <input type="text" placeholder="جستوجو" />
-                    </div>
-                </div>
-                <div className="Search-Modal-Result-Box">
+                <div className={`Search-Result-Box ${!Empty ? "Show-Result-Box" : ""}`}>
 
                 </div>
             </div>
