a4eb3b063870eca510357746f640707eba15e278
mohammadmahdi-khaje <166861811+mohammadmahdi-khaje@users.noreply.github.com>
2025-05-02 20:31:59 +0330

    home page fix

--- src/App/App.js
+++ src/App/App.js
@@ -4,20 +4,25 @@ import "./AppStyle.scss";
 // Import Component
 import Navigation from "../component/navigation/Navigatin";
 import HomePage from "../component/HomePage/HomePage";
-import Cart from "../component/All Item/cart/Cart";
-import GameList from "../component/All Item/GameList/GameList";
-import Slideshow from "../component/All Item/SlideShow/SlideShow";
 
 const App = () => {
   const [NavigationStatus, setNavigationStatus] = useState(false);
   return (
     <>
       <div className="App-Background">
-        <div className={`App-Navigation  ${NavigationStatus ? "Open-App-Navigation" : "Close-App-Navigation"}`}>
+        <div
+          className={`App-Navigation  ${
+            NavigationStatus ? "Open-App-Navigation" : "Close-App-Navigation"
+          }`}
+        >
           <Navigation setNavigationStatus={setNavigationStatus} />
         </div>
-        <div className={`App-Content  ${NavigationStatus ? "Open-App-Content" : "Close-App-Content"}`}>
-          <HomePage/>
+        <div
+          className={`App-Content  ${
+            NavigationStatus ? "Open-App-Content" : "Close-App-Content"
+          }`}
+        >
+          <HomePage />
         </div>
       </div>
     </>
--- src/component/HomePage/HomePage.jsx
+++ src/component/HomePage/HomePage.jsx
@@ -3,15 +3,26 @@ import "./HomePageStyle.scss";
 
 // Import Component
 import Slideshow from "../All Item/SlideShow/SlideShow";
+import GameList from "../All Item/GameList/GameList";
+import CartSlideShow from "../All Item/CartSlideShow/CartSlideShow";
 
 const HomePage = () => {
-    return (
-        <div className="Home-Page-Background">
-            <div className="Home-Page-Slide-Show">
-                <Slideshow/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="Home-Page-Background">
+      <p>مهبوب ترین ها</p>
+      <div className="Home-Page-Slide-Show">
+        <Slideshow />
+      </div>
+      <p>جدید ترین ها</p>
+      <div className="Home-Page-Cart-Slide-Show">
+        <CartSlideShow />
+      </div>
+      <p>همه بازی ها</p>
+      <div className="Home-Page-GameList">
+        <GameList />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
