d40e8abac39fa5c82a062bd8da581bff882e8c73
jackrichar <jackrichardsaleh@gmail.com>
2025-05-13 17:59:27 +0330

    Archive Page V-2

--- src/component/ArchivePage/Filter/Filter.jsx
+++ src/component/ArchivePage/Filter/Filter.jsx
@@ -6,7 +6,6 @@ import "./FilterStyle.scss";
 import CustomSelectBox from "../../All Item/CustomSelectBox/CustomSelectBox";
 
 // Import SVG
-import {ReactComponent as ArrowIco} from "../../../Assets/Icon/Arrow.svg";
 import {ReactComponent as FilterIco} from "../../../Assets/Icon/Filter.svg";
 
 const Filter = () => {
@@ -28,9 +27,17 @@ const Filter = () => {
                 <div className="Archive-Page-Filter-Item">
                     <CustomSelectBox
                         Placeholder={"ژانر"}
-                        Value={["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Romance", "Sci-Fi", "Thriller", "Western"]}
+                        Value={["RPG", "Fighting", "Simulation", "Puzzle", "Sports", "Adventure", "Platformer", "Sandbox", "Shooter", "Survival horror"]}
                         onChange={HandleSelect}
                         Multiple={true}
+                    />
+                </div>
+                <div className="Archive-Page-Filter-Item">
+                    <CustomSelectBox
+                        Placeholder={"سال انتشار"}
+                        Value={["1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008"]}
+                        onChange={HandleSelect}
+                        Multiple={false}
                     />
                 </div>
             </div>
